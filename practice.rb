d3d63cdbff83237ccebc4e6bb4566002dbd0dd30
Joshua Yee <joshyee16@gmail.com>
2023-10-30 15:51:02 -0700

    10/30/23 daily practice

---
+++ practice.rb
@@ -0,0 +1,37 @@
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+
+number = 10
+puts number + 10
+
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "dog"
+puts word.upcase
+
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+num1 = 7
+num2 = 3
+puts num1 + num2
+
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+reverse_word = "hello"
+puts reverse_word.reverse
+
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+number = 5
+puts number * 10
+
+
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+var = "KitChen"
+puts var.downcase
+
+
