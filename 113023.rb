da9009bafe65e44bfd27f3a031ac2680e3170d38
Joshua Yee <joshyee16@gmail.com>
2023-12-01 17:39:23 -0800

    shoppingcart class and Product class exercises completed

---
+++ 113023.rb
@@ -0,0 +1,59 @@
+# CUSTOM CLASSES AND METHODS 4 (1-4)
+
+# Write a ShoppingCart class that stores an array of items with methods to add an item, remove an item, and display all the items.
+
+class ShoppingCart
+  def initialize
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+
+  def remove_item(item)
+    @items.delete(item)
+  end
+
+  def display_items
+    puts "Items in the shopping cart:"
+    @items.each { |item| puts "- #{item}" }
+  end
+end
+
+cart = ShoppingCart.new
+cart.add_item("apple")
+cart.add_item("banana")
+cart.add_item("orange")
+cart.add_item("celery")
+cart.display_items
+cart.remove_item("celery")
+cart.display_items
+
+
+# Write a Product class that stores the name, price, and metadata, where metadata is a hash that stores additional information about the product.
+
+class Product
+  attr_reader :name, :price, :metadata
+  attr_writer :name, :price, :metadata
+
+  def initialize(opts)
+    @name = opts[:name]
+    @price = opts[:price]
+    @metadata = opts[:metadata]
+  end 
+end
+
+laptop = Product.new(name: "MacBook", price: 1000, metadata: {color: "gray", model: "Pro", year: "2020"})
+p laptop.name
+puts "$#{laptop.price}" 
+puts laptop.metadata[:color]
+puts laptop.metadata[:model]
+puts "This laptop was released in #{laptop.metadata[:year]}."
+
+# Write a Playlist class that stores a name and an array of songs with methods to add a song, remove a song, shuffle the songs into a random order, and display all the songs.
+
+
+
+# Write a Contact class that stores the name, age, and contact_info, where contact_info is a hash that stores any additional information about the contact.
+
