43a63d90949435c18902857432a9ab9619fa603d
Joshua Yee <joshyee16@gmail.com>
2023-12-01 18:12:29 -0800

    create Playlist class with add and remove song methods, no shuffle or display

--- 12123.rb
+++ 12123.rb
@@ -53,7 +53,28 @@ puts "This laptop was released in #{laptop.metadata[:year]}."
 
 # Write a Playlist class that stores a name and an array of songs with methods to add a song, remove a song, shuffle the songs into a random order, and display all the songs.
 
+class Playlist
+  attr_reader :name, :songs
+  attr_writer :name, :songs
 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    songs << song
+  end
+
+  def remove_song(song)
+    songs.delete(song)
+  end
+end
+
+playlist = Playlist.new("Study Playlist")
+p playlist.add_song("Cornfield Chase")
+p playlist.add_song("Mountains")
+p playlist.add_song("Now We Are Free")
 
 # Write a Contact class that stores the name, age, and contact_info, where contact_info is a hash that stores any additional information about the contact.
 
