aa8ce342a2f228dbd6e3b6fd96450d80cd0d92da
Joshua Yee <joshyee16@gmail.com>
2023-12-13 22:39:58 -0800

    conditionals 1 (1-10) in JS

---
+++ 121323.js
@@ -0,0 +1,116 @@
+// Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+let number = 8
+
+if (number === 18) {
+  console.log(0)
+} else {
+  console.log(-1)
+}
+
+
+// Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+let num = 10
+
+if (num < 10) {
+  console.log(-1)
+} else if (num > 10) {
+  console.log(1)
+} else {
+  console.log(0)
+}
+
+// Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+let num1 = 3
+let num2 = 5
+
+if (num1 < 10 && num2 < 10) {
+  console.log(1)
+} else {
+  console.log(0)
+}
+
+// Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+let n = 8
+
+if (n > 9000) {
+  console.log(1)
+} else {
+  console.log(-1)
+}
+
+// Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+let bigBoyNum = 8383
+
+if (bigBoyNum < 10) {
+  console.log(9)
+} else if (bigBoyNum < 20) {
+  console.log(19)
+} else if (bigBoyNum < 30) {
+  console.log(29)
+} else {
+  console.log(-1)
+}
+
+// Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+n1 = 83
+n2 = 8
+
+if (n1 > 10 || n2 > 10) {
+  console.log(100)
+} else {
+  console.log(-100)
+}
+
+// Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+let year = 2005
+
+if (year < 0) {
+  console.log(1776)
+} else {
+  console.log(1979)
+}
+
+// Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+let numero = 100
+if (numero === 100) {
+  console.log(100)
+} else if (numero === 99) {
+  console.log(99)
+} else {
+  console.log(0)
+}
+
+// Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+let x = -9
+let y = 3
+
+if (x < 0 && y > 0) {
+  console.log(1)
+} else {
+  console.log(0)
+}
+
+// Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+let counter = 9
+
+if (counter > 80) {
+  console.log(5)
+} else if (counter > 60) {
+  console.log(4)
+} else if (counter > 40) {
+  console.log(3)
+} else if (counter > 20) {
+  console.log(2)
+} else {
+  console.log(1)
+}
