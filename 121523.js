38e7ffeb3f869c5315e1659c1b9546c7d0c53bf5
Joshua Yee <joshyee16@gmail.com>
2023-12-18 00:26:18 -0800

    js loops 1 (1-10)

---
+++ 121523.js
@@ -0,0 +1,101 @@
+// Write a while loop to print the numbers 1 through 10.
+
+// let i = 1
+// while (i <= 10) {
+//   console.log(i)
+//   i++
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i)
+// }
+
+// Write a while loop that prints the word "hello" 5 times.
+
+// let i = 0
+// while (i < 5) {
+//   console.log("hello")
+//   i++
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("hello")
+// }
+
+
+// Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+// while (true) { 
+//   let input = window.prompt("Enter a word:")
+//   if (input === "stop") {
+//     break
+//   }
+// }
+
+
+// Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+// let i = 0
+// while (i <= 100) {
+//   console.log(i)
+//   i += 5
+// }
+
+// Write a while loop that prints the number 9000 ten times.
+
+// let i = 1
+// while (i <= 10) {
+//   console.log(9000)
+//   i++
+// }
+
+// Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// while (true) {
+//   let input = window.prompt("Enter a number:")
+//   if (input > 10) {
+//     break
+//   }
+// }
+
+// Write a while loop that prints the numbers 50 to 70.
+
+// let i = 50
+// while (i < 71) {
+//   console.log(i)
+//   i++
+// }
+
+// Write a while loop that prints the phrase "Around the world" 144 times.
+
+// let i = 0
+// while (i < 144) {
+//   console.log("Around the world")
+//   i ++
+// }
+
+// Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+// while (true) {
+//   let input = window.prompt("Enter a word:")
+//   if (input.length > 5) {
+//     break
+//   }
+// }
+
+
+// Write a while loop that prints the even numbers from 2 to 40.
+
+// let i = 1
+// while (i <= 40) {
+//   if (i % 2 === 0) {
+//     console.log(i)
+//   }
+//   i++
+// }
+
+// while (i <= 40) {
+//   console.log(i)
+//   i += 2
+// }
+
