25a4a584f9fbd02942068f68780095152b6692ee
Joshua Yee <joshyee16@gmail.com>
2023-12-19 19:11:07 -0800

    js loops 2 (6-10)

---
+++ 121923.js
@@ -0,0 +1,105 @@
+// Start with an array of hashes and create a new array of number values from each hash's :age key.
+
+// let people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+// let ages = []
+
+// WHILE
+// let i = 0
+// while (i < people.length) {
+//   let person = people[i]
+//   ages.push(person["age"])
+//   i++
+// }
+// console.log(ages)
+
+// ForEach
+// people.forEach(person => ages.push(person["age"]))
+// console.log(ages)
+
+// MAP
+// let ages = people.map(person => person["age"])
+// console.log(ages)
+
+// Start with an array of numbers and create a new array with each number divided by 2.
+
+// let numbers = [1, 2, 3]
+// let halvedNums = []
+
+// WHILE
+// let i = 0
+// while (i < numbers.length) {
+//   halvedNums.push(numbers[i] / 2)
+//   i++
+// }
+// console.log(halvedNums)
+
+// ForEach
+// numbers.forEach(number => halvedNums.push(number / 2))
+// console.log(halvedNums)
+
+// MAP
+// let halvedNums = numbers.map(number => number / 2)
+// console.log(halvedNums)
+
+
+// Start with an array of strings and create a new array with each string's first letter only.
+
+let strings = ["hello", "goodbye"]
+// let firstLetters = []
+
+// WHILE
+// let i = 0
+// while (i < strings.length) {
+//   firstLetters.push(strings[i][0])
+//   i++
+// }
+// console.log(firstLetters)
+
+// ForEach
+// strings.forEach(string => firstLetters.push(string[i]))
+// console.log(firstLetters)
+
+// MAP
+// let firstLetters = strings.map(string => string[0])
+// console.log(firstLetters)
+
+
+// Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
+
+// let people = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+// let agesTimesTwo = []
+
+// WHILE
+// let i = 0
+// while (i < people.length) {
+//   agesTimesTwo.push(people[i]["age"] * 2)
+//   i++
+// } 
+// console.log(agesTimesTwo)
+
+// ForEach
+// people.forEach(person => agesTimesTwo.push(person["age"] * 2))
+// console.log(agesTimesTwo)
+
+// MAP
+// let agesTimesTwo = people.map(person => person["age"] * 2)
+// console.log(agesTimesTwo)
+
+// Start with an array of numbers and create a new array with each number converted into a string.
+// For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+// let numbers = [1, 2, 3]
+// let stringedNumbers = []
+
+// WHILE
+// let i = 0
+// while (i < numbers.length) {
+//   let number = numbers[i]
+//   stringedNumbers.push(number.toString())
+//   i++
+// }
+// console.log(stringedNumbers)
+
+// ForEach
+// numbers.forEach(number => stringedNumbers.push(number.toString()))
+// console.log(stringedNumbers)
