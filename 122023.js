9eefc3f92ac989b6e98e0a8e8937e3b6ecf4de03
Joshua Yee <joshyee16@gmail.com>
2023-12-20 18:54:41 -0800

    js loops 3 (1-6)

---
+++ 122023.js
@@ -0,0 +1,165 @@
+// Start with an array of numbers and create a new array with only the numbers less than 20.
+
+// let numbers = [2, 32, 80, 18, 12, 3]
+// let smallNumbers = []
+
+// WHILE
+// let i = 0
+// while (i < numbers.length) {
+//   let number = numbers[i]
+//   if (number < 20) {
+//     smallNumbers.push(number)
+//   } i++
+// }
+// console.log(smallNumbers)
+
+// FOREACH
+// numbers.forEach(number => {
+//   if (number < 20) {
+//     smallNumbers.push(number)
+//   }
+// })
+// console.log(smallNumbers)
+
+// FILTER
+// let smallNumbers = numbers.filter(number => number < 20)
+// console.log(smallNumbers)
+
+// Start with an array of strings and create a new array with only the strings that start with the letter "w".
+
+// let words = ["winner", "winner", "chicken", "dinner"]
+// let wWords = []
+
+// WHILE
+// let i = 0
+// while (i < words.length) {
+//   if (words[i][0] === "w") {
+//     wWords.push(words[i])
+//   } i++
+// }
+// console.log(wWords)
+
+// ForEach
+// words.forEach(word => {
+//   if (word[0] === "w") {
+//     wWords.push(word)
+//   }
+// })
+// console.log(wWords)
+
+// // FILTER
+// let wWords = words.filter(word => word[0] === "w")
+// console.log(wWords)
+
+// Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
+
+// let items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}, {name: "eraser", price: 6}]
+// let expensiveItems = []
+
+// WHILE
+// let i = 0
+// while (i < items.length) {
+//   if (items[i]["price"] > 5) {
+//     expensiveItems.push(items[i])
+//   } i++
+// }
+// console.log(expensiveItems)
+
+// ForEach
+// items.forEach(item => {
+//   if (item["price"] > 5) {
+//     expensiveItems.push(item)
+//   }
+// })
+// console.log(expensiveItems)
+
+// FILTER 
+// let expensiveItems = items.filter(item => item["price"] > 5)
+// console.log(expensiveItems)
+
+
+// Start with an array of numbers and create a new array with only the even numbers.
+
+// let numbers = [2, 4, 5, 1, 8, 9, 7]
+// let evenNumbers = []
+
+// WHILE
+// let i = 0
+// while (i < numbers.length) {
+//   let num = numbers[i]
+//   if (num % 2 === 0) {
+//     evenNumbers.push(num)
+//   } i++
+// }
+// console.log(evenNumbers)
+
+// ForEach
+// numbers.forEach(num => {
+//   if (num % 2 < 1) {
+//     evenNumbers.push(num)
+//   }
+// })
+// console.log(evenNumbers)
+
+// FILTER
+// let evenNumbers = numbers.filter(num => num % 2 === 0)
+// console.log(evenNumbers)
+
+
+// Start with an array of strings and create a new array with only the strings shorter than 4 letters.
+
+// let strings = ["a", "man", "a", "plan", "a", "canal", "panama"]
+// let shortStrings = []
+
+// WHILE
+// let i = 0
+// while (i < strings.length) {
+//   if (strings[i].length < 4) {
+//     shortStrings.push(strings[i])
+//   } i++
+// }
+// console.log(shortStrings)
+
+// ForEach
+// strings.forEach(function(string) {
+//   if (string.length < 4) {
+//     shortStrings.push(string)
+//   }
+// })
+// console.log(shortStrings)
+
+// FILTER
+// let shortStrings = strings.filter(str => str.length < 4)
+// console.log(shortStrings)
+
+
+// Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
+
+let items = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}, {name: "eraser", price: 6}]
+// let shortNames = []
+
+// WHILE
+// let i = 0
+// while (i < items.length) {
+//   if (items[i]["name"].length < 6) {
+//     shortNames.push(items[i])
+//   } i++
+// }
+// console.log(shortNames)
+
+// ForEach
+// items.forEach(item => {
+//   if (item["name"].length < 6) {
+//     shortNames.push(item)
+//   }
+// })
+// console.log(shortNames)
+
+// FILTER
+// let shortNames = items.filter(item => item["name"].length < 6)
+// console.log(shortNames)
+
+
+
+
+
