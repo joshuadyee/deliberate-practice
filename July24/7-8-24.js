d18c4bf6443e204a836c72c8e211e119334909c6
Joshua Yee <joshyee16@gmail.com>
2024-07-08 09:49:17 -0700

    Prodecural solution for LC Contains Duplicates

---
+++ July24/7-8-24.js
@@ -0,0 +1,23 @@
+// LC Contains Duplicate
+
+function containsDuplicate(nums) {
+  // if nums contains a duplicate => true
+  // no duplicate => false
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]
+    for (let j = 0; j < nums.length; j++) {
+      // console.log(nums[i], nums[j])
+      if (i === j) { // same index, skip
+      } else if (nums[i] === nums[j]) {
+        return true
+      } 
+    }
+  }
+  return false
+}
+
+console.log(containsDuplicate([1,2,3]))
+console.log(containsDuplicate([1,2,3,1]))
+console.log(containsDuplicate([1,2,3,4,5,6,7,8]))
+
+// is a valid solution, exceeds time limit for Leetcode editor
