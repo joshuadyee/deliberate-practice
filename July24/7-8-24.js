a74dee95fbe7ecbab7bbd35c3a74087706c6ec5b
Joshua Yee <joshyee16@gmail.com>
2024-07-09 11:34:53 -0700

    Finish solution for Contains Duplicates

--- July24/7-8-24.js
+++ July24/7-8-24.js
@@ -16,8 +16,33 @@ function containsDuplicate(nums) {
   return false
 }
 
+// console.log(containsDuplicate([1,2,3]))
+// console.log(containsDuplicate([1,2,3,1]))
+// console.log(containsDuplicate([1,2,3,4,5,6,7,8]))
+
+// is a valid solution, exceeds time limit for Leetcode editor
+
+
+function containsDuplicate(nums) {
+  let hash = {}
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i]
+    if (hash[num] === undefined) {
+      hash[num] = 0 
+    } 
+    hash[num]++
+  }
+  // console.log(hash)
+  // console.log(Object.values(hash))
+  const values = Object.values(hash)
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] > 1) {
+      return true
+    }
+  }
+  return false
+}
+
 console.log(containsDuplicate([1,2,3]))
 console.log(containsDuplicate([1,2,3,1]))
 console.log(containsDuplicate([1,2,3,4,5,6,7,8]))
-
-// is a valid solution, exceeds time limit for Leetcode editor
